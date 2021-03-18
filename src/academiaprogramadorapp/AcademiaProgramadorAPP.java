a98a54ff7b5c77a05f4aafff85776f7d83a158e2
Matheus <matheusdefigueiredo@gmail.com>
2021-03-18 14:22:54 -0300

    Versão finalizada e com comentários - APP NDD

--- src/academiaprogramadorapp/AcademiaProgramadorAPP.java
+++ src/academiaprogramadorapp/AcademiaProgramadorAPP.java
@@ -5,7 +5,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-
+/*Esta é uma aplicação, para ajudar na organização de equipamentos.
+Nele pode-se registrar ou excluir um ou vários equipamentos, agendar e cancelar manutenções, visualizar informações do equipamento e suas chamads de manutenção.
+Para o funcionamento, foram usadas 3 classes, sendo uma para execução, uma para o usuário, que acessa uma terceira, feita para os equipamentos.
+A interação se dá, por meio do acesso pelo usuário à uma lista "array", com a interface gráfica JOptionPane, da biblioteca SWING.
+A aplicação se dá por meio de informações digitadas pelo usuário. Suas escolhas geram alterações, que alteram as configurações dos equipamentos e suas manutenções.
+As utilidades de cada método, estão descritas acima de cada uma, no código abaixo.*/
 public class AcademiaProgramadorAPP {
 
 
@@ -36,6 +41,7 @@ public class AcademiaProgramadorAPP {
         
         int var = 8;
         
+        //Por meio desta função, pode-se visualizar as informações de cada equipamento resgistrado.
             do{
             var = Integer.parseInt(JOptionPane.showInputDialog(null, " Digite uma das seguintes opções: \n 1 - Visualizar \n 2 -  Editar equipamento \n 3 - Excluir um equipamento \n 4 - Colocar em manutenção \n 5 - Ver chamadas de manutenção \n 6 - Editar chamada de manutenção \n 7 - Excluir chamada \n 0 - Finalizar programa "));
             if(var==1){
@@ -44,34 +50,45 @@ public class AcademiaProgramadorAPP {
                 
                 user.verificar(e1);
             }
+            
+            //A função "editar()", serve para permitir que o usuário altere as informações de um equipamento já resgistrado.
             else if(var==2){
                 String e1 = (JOptionPane.showInputDialog(null, "Qual o nome do equipamento? "));
                 user.editar(e1);
             }
+            
+            //Aqui é onde um equipamento é removido da lista.
             else if(var==3){
                 
                 String e1 = (JOptionPane.showInputDialog(null, "Qual o nome do equipamento que deseja remover? "));
                 if(listaequipamentos.size() == 1){
-                    JOptionPane.showMessageDialog(null, "Você não possui mais equipamentos registrados. \nFinalizando aplicativo...");
+                    JOptionPane.showMessageDialog(null, "Você você removeu todos equipamentos registrados. \nFinalizando aplicativo...");
                     var=0;
                 }
                 else if(listaequipamentos.size() != 1){
                 user.remover(e1);
                 }
             }
+            
+            //As manutenções são agendadas por esta parte do código.
             else if(var==4){
                 String e1 = (JOptionPane.showInputDialog(null, "Qual o nome do equipamento que deseja colocar em manutenção? "));
                 user.manutençãoon(e1);
             }
             
+            //Caso necessário, pode-se verificar as manutenções, que ocorre por meio da função "verificamanut()".
             else if(var==5){
                 String e1 = (JOptionPane.showInputDialog(null, "Ei, me diz o nome do equipamento, pra gente ver se ele tem manutenção marcada: "));
                 user.verificarmanut(e1);
             }
+            
+            //Se quiser editaruma manutenção, esta parte docódigo faz isso. 
             else if(var==6){
                 String e1 = (JOptionPane.showInputDialog(null, "Qual o nome do equipamento que deseja colocar em manutenção? "));
                 user.editmanut(e1);
             }
+            
+            //Por fim, encerra-se o programa aqui.
             else if(var==7){
                 String e1 = JOptionPane.showInputDialog(null, "Me diz, a manutenção de qual equipamento você quer que eu desmarque? ");
                 user.removemanut(e1);
